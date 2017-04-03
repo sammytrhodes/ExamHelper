05dd64c4697e7574bac1fd610a6ecdc2450a67a9
Liam <lbarry@ithaca.edu>
2017-04-03 09:18:31 -0400

    added some stuff, deleted some stuff

--- QuestionChoiceInterface.java
+++ QuestionChoiceInterface.java
@@ -5,9 +5,9 @@
  */
 package examhelper;
 
-import javafx.geometry.Insets;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -22,12 +22,12 @@ import javafx.stage.Stage;
  *
  * @author liambarry
  */
-public class ChoiceInterface extends Application {
+public class QuestionChoiceInterface extends Application{
     
     @Override
     public void start(Stage primaryStage) {
         
-        primaryStage.setTitle("Choice Interface");
+        primaryStage.setTitle("Question Choice");
         
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -41,55 +41,31 @@ public class ChoiceInterface extends Application {
         Text scenetitle = new Text("Please Choose an Option");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
-
-        //Label userName = new Label("User Name:");
-        //grid.add(userName, 0, 1);
-
-        //TextField userTextField = new TextField();
-        //grid.add(userTextField, 1, 1);
-
-        //Label pw = new Label("Password:");
-        //grid.add(pw, 0, 2);
-
-        //PasswordField pwBox = new PasswordField();
-        //grid.add(pwBox, 1, 2);
         
-        Button btn = new Button("Export Exam");
+        Button btn = new Button("Import Single Question");
         HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.CENTER);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
         
-        Button btn2 = new Button("Import Questions");
+        Button btn2 = new Button("Import From File");
         HBox hbBtn2 = new HBox(10);
         hbBtn2.setAlignment(Pos.BOTTOM_LEFT);
         hbBtn2.getChildren().add(btn2);
         grid.add(hbBtn2, 0, 4);
-        
-        Button btn3 = new Button("Log out");
-        HBox hbBtn3 = new HBox(10);
-        hbBtn3.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn3.getChildren().add(btn3);
-        grid.add(hbBtn3, 2, 4);
+       
 
         primaryStage.show();
   
-        btn.setOnAction((ActionEvent e) -> {
+          btn.setOnAction((ActionEvent e) -> {
             System.out.println("clicked");
         });
         
         btn2.setOnAction((ActionEvent e) -> {
             System.out.println("clicked");
         });
-        
-        btn3.setOnAction((ActionEvent e) -> {
-            System.out.println("clicked");
-        });
+     
     }
-    
-    //public static String nextScreen(String screen){
-        
-    //}
 
     /**
      * @param args the command line arguments
@@ -97,5 +73,4 @@ public class ChoiceInterface extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
